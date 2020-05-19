df0fe6e0d8333b3b60d3f982e02526aaa98d712a
Don Grote <don.grote@gmail.com>
2020-05-19 10:29:40 -0400

    wip

---
+++ nfc-pcsc.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const NFC = require('nfc-pcsc');
+const nfc = new NFC();
+
+nfc
+  .on('reader', reader => {
+    reader
+      .on('error', err => console.error('reader error', err))
+      .on('end', () => console.log(`${reader.reader.name} device removed`))
+      .on('card', card => console.log(`card inserted`, card))
+      .on('card.off', card => console.log(`card removed`, card));
+  })
+  .on('error', err => console.error('nfc error', err));
