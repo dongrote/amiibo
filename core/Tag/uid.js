bb30121acea9fe81271682d54b5e1f3bd12ee55a
Don Grote <don.grote@gmail.com>
2020-05-19 14:29:43 -0400

    wip

--- core/Tag/uid.js
+++ core/Tag/uid.js
@@ -2,4 +2,4 @@
 /**
  * remove the checksum bytes from the first two pages of a tag to get the uid
  */
-exports = module.exports = page => page.slice(7, 7+8);
+exports = module.exports = page => Buffer.concat([page.slice(0, 3), page.slice(4, 8)]);
