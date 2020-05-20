ecc72c15f9a399c5b5128088e82a4173d8b24cd6
Don Grote <don.grote@gmail.com>
2020-05-20 14:56:56 -0400

    wip

--- nfc-pcsc.js
+++ nfc-pcsc.js
@@ -7,30 +7,25 @@ const core = require('./core');
 
 const db = new core.AmiiboDatabase();
 
-db.load('./amiibo.json')
-  .then(() => {
-    console.log('amiibo database loaded');
-    nfc
-      .on('reader', reader => {
-        reader
-          .on('error', err => console.error('reader error', err))
-          .on('end', () => console.log(`${reader.reader.name} device removed`))
-          .on('card', card => {
-            console.log(`card inserted`, card);
-            reader.read(0, 540)
-              .then(data => {
-                const amiibo = new core.Amiibo(data);
-                console.log('uid', amiibo.uid().toString('hex'));
-                console.log('amiibo id', amiibo.amiiboId().toString('hex'));
-                console.log('amiibo: ', db.lookupAmiiboById(amiibo.amiiboId().toString('hex')));
-              })
-              .catch(err => console.error('read error', err));
+db.load('./amiibo.json').catch(console.error);
+    
+
+nfc
+  .on('reader', reader => {
+    reader
+      .on('error', err => console.error('reader error', err))
+      .on('end', () => console.log(`${reader.reader.name} device removed`))
+      .on('card', card => {
+        console.log(`card inserted`, card);
+        reader.read(0, 540)
+          .then(data => {
+            const amiibo = new core.Amiibo(data);
+            console.log('uid', amiibo.uid().toString('hex'));
+            console.log('amiibo id', amiibo.amiiboId().toString('hex'));
+            console.log('amiibo: ', db.lookupAmiiboById(amiibo.amiiboId().toString('hex')));
           })
-          .on('card.off', () => console.log(`card removed`));
-    })
-    .on('error', err => console.error('nfc error', err));
-  })
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
+          .catch(err => console.error('read error', err));
+      })
+      .on('card.off', () => console.log(`card removed`));
+})
+.on('error', err => console.error('nfc error', err));
