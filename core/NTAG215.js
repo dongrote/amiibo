c28ce89776bd136868850c0f2997188ce566e5d2
Don Grote <don.grote@gmail.com>
2020-05-27 09:02:08 -0400

    wip

--- core/NTAG215.js
+++ core/NTAG215.js
@@ -8,14 +8,24 @@ class NTAG215 extends EventEmitter {
   }
 
   async serialNumber() {
-    return await this.reader.read(0, 9);
+    const read = await this.reader.read(0, 9);
+    return Buffer.concat([
+      read.slice(0, 3),
+      read.slice(4, 8),
+    ]);
+  }
+
+  async lockBytes() {
+    const read = await this.reader.read(2, 4);
+    return read.slice(2, 4);
   }
 
   capabilities() {
 
   }
 
-  pageIsLocked(pageno) {
+  async pageIsLocked(pageno) {
+    const lockBytes = await this.lockBytes();
 
   }
 
