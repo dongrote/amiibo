30f3f8a4e10a057fde1aa161576346a6c2b57a96
Don Grote <don.grote@gmail.com>
2020-05-28 11:49:52 -0400

    wip

--- nfc-pcsc-file.js
+++ nfc-pcsc-file.js
@@ -8,14 +8,21 @@ const db = new core.AmiiboDatabase();
 db
   .load('./amiibo.json')
   .then(() => {
-    const reader = new core.FileTagReader();
+    const reader = new core.FileTagReader(),
+      writer = new core.FileTagReader();
     return reader
       .import(process.argv[2])
       .then(() => {
         const amiibo = new core.Amiibo(reader);
         return amiibo.id()
           .then(id => db.lookupAmiiboById(id))
-          .then(a => console.log(`amiibo: ${_.get(a, 'name', 'unknown')}`));
+          .then(a => console.log(`amiibo: ${_.get(a, 'name', 'unknown')}`))
+          .then(() => {
+            const amiiboExport = new core.Amiibo(writer);
+            return amiiboExport.write(reader.buffer)
+              .then(() => amiiboExport.export(process.argv[3]))
+              .then(() => console.log('exported new amiibo to ', process.argv[3]));
+          });
       });
   })
   .catch(console.error);
