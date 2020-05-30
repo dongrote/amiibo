459d6d2d96765100e034a52555cc05ab7ef58056
Don Grote <don.grote@gmail.com>
2020-05-30 14:22:31 -0400

    wip

--- core/AmiiboDatabase/index.js
+++ core/AmiiboDatabase/index.js
@@ -9,26 +9,3 @@ fs.readdirSync(__dirname)
   .forEach(fname => {
     exports[fname] = require(`./${fname}`);
   });
-  'use strict';
-  const fs = require('fs'),
-    path = require('path');
-  exports = module.exports = {};
-  
-  fs.readdirSync(__dirname)
-    .filter(fname => fname !== path.basename(__filename))
-    .map(fname => path.basename(fname, '.js'))
-    .forEach(fname => {
-      exports[fname] = require(`./${fname}`);
-    });
-'use strict';
-const fs = require('fs'),
-  path = require('path');
-exports = module.exports = {};
-
-fs.readdirSync(__dirname)
-  .filter(fname => fname !== path.basename(__filename))
-  .map(fname => path.basename(fname, '.js'))
-  .forEach(fname => {
-    exports[fname] = require(`./${fname}`);
-  });
-  
