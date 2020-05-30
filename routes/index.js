1e761e9e7289a008561bce33172e89335ed63504
Don Grote <don.grote@gmail.com>
2020-05-30 12:19:47 -0400

    wip

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
 'use strict';
 exports = module.exports = require('express').Router();
 
-router.get('/health', (req, res) => res.sendStatus(200));
+exports.get('/health', (req, res) => res.sendStatus(200));
