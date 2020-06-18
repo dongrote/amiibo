d07bf3c772ff47337d05af83f1e4398642199acc
Don Grote <don.grote@gmail.com>
2020-06-18 17:19:20 -0400

    wip

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 'use strict';
 exports = module.exports = require('express').Router();
 const systemState = require('./systemState'),
+  amiibos = require('./amiibos'),
   setPurpose = require('./setPurpose');
 
 exports.get('/health', (req, res) => res.sendStatus(200));
+exports.get('/amiibos', amiibos);
 exports.get('/system/state', systemState);
 exports.post('/system/purpose', setPurpose);
